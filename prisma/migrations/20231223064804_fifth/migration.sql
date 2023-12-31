46e9ee65893ab1299e47f179b4c54e4bfddaeb29
muthuri-dev <muthurikennedy082@gmail.com>
2024-01-01 01:36:10 +0300

    database migration

--- prisma/migrations/20231223064804_fifth/migration.sql
+++
@@ -1,9 +0,0 @@
--- CreateTable
-CREATE TABLE "Follower" (
-    "id" TEXT NOT NULL,
-    "follower" INTEGER NOT NULL,
-    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    "updatedAt" TIMESTAMP(3) NOT NULL,
-
-    CONSTRAINT "Follower_pkey" PRIMARY KEY ("id")
-);
