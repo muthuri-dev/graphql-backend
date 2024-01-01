3e1b1c8c2cc2d3482eb61261e06450402eafc8b9
muthuri-dev <muthurikennedy082@gmail.com>
2024-01-02 02:17:06 +0300

    Create navbar component

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,17 @@
+import Navbar from "@/components/shared/Navbar";
 import React from "react";
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center p-24">
-      <h1 className="underline text-center">List of Users</h1>
+      <Navbar />
+      <div className="mockup-browser border border-base-300 w-full">
+        <div className="mockup-browser-toolbar">
+          <div className="input border border-base-300">Hello developers</div>
+        </div>
+        <div className="flex justify-center px-4 py-16 border-t border-base-300">
+          Hello!
+        </div>
+      </div>
     </main>
   );
 }
