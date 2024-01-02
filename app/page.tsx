be2c322980653e06e9e38a5e188ed66a36150ea1
muthuri-dev <muthurikennedy082@gmail.com>
2024-01-02 04:36:17 +0300

    Create homepage

--- app/page.tsx
+++ app/page.tsx
@@ -11,15 +11,44 @@ export default function Home() {
         </div>
         <div className="grid md:grid-cols-5 border-t border-base-300">
           <div className="col-span-5 md:col-span-3 flex justify-center items-center text-center flex-col">
-            <h1 className="font-extrabold text-white text-6xl italic">
+            <h1 className="font-extrabold text-white text-6xl italic ml-20 mr-20">
               Learn and teach in one platform
             </h1>
             <p className="text-gray-700 font-bold text-xl">
               ...thoughts? share them here
             </p>
+            <button className="btn glass">Go to Blogs</button>
           </div>
-          <div className=" md:col-span-2 flex justify-center p-24">
-            <Image src="posting.svg" height={500} width={500} alt="image" />
+          <div className="hidden md:col-span-2 md:flex justify-center p-24">
+            <div className="stack">
+              <div className="card shadow-md bg-primary text-primary-content">
+                <div className="card-body">
+                  <h2 className="card-title">Notification 1</h2>
+                  <p>You have 3 unread messages. Tap here to see.</p>
+                </div>
+              </div>
+              <div className="card shadow bg-primary text-primary-content">
+                <div className="card-body">
+                  <h2 className="card-title">Notification 2</h2>
+                  <p>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                    Eius ea amet perspiciatis dolore. Omnis, in molestiae esse
+                    quasi doloremque ut minus
+                  </p>
+                </div>
+              </div>
+              <div className="card shadow-sm bg-primary text-primary-content">
+                <div className="card-body">
+                  <h2 className="card-title">Technology</h2>
+                  <p>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                    Nisi minus inventore, harum voluptatum porro vero odio
+                    consequuntur quas quod. Labore omnis tempora magnam illo
+                    pariatur blanditiis a. Possimus, veritatis atque.
+                  </p>
+                </div>
+              </div>
+            </div>
           </div>
         </div>
       </div>
--- components/shared/Navbar.tsx
+++ components/shared/Navbar.tsx
@@ -1,12 +1,69 @@
+import Link from "next/link";
 import React from "react";
+import { TfiWrite } from "react-icons/tfi";
 
 export default function Navbar() {
   return (
-    <div className="navbar bg-slate-950 rounded-2xl mb-10">
-      <div className="flex-1">
+    <div className="navbar bg-slate-950 rounded-2xl mb-10 flex justify-between sticky">
+      <div className="flex">
         <a className="btn btn-ghost text-xl">Bloggz</a>
       </div>
+      <div>
+        <Link
+          href="#"
+          className="hover:underline text-white font-bold italic mr-5"
+        >
+          Home
+        </Link>
+        <Link
+          href="#"
+          className="hover:underline text-white font-bold italic mr-5"
+        >
+          Blogs
+        </Link>
+      </div>
       <div className="flex-none">
+        <div className="mr-4">
+          <label className="cursor-pointer grid place-items-center">
+            <input
+              type="checkbox"
+              value="night"
+              className="toggle theme-controller bg-base-content row-start-1 col-start-1 col-span-2"
+            />
+            <svg
+              className="col-start-1 row-start-1 stroke-base-100 fill-base-100"
+              xmlns="http://www.w3.org/2000/svg"
+              width="14"
+              height="14"
+              viewBox="0 0 24 24"
+              fill="none"
+              stroke="currentColor"
+              strokeWidth="2"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+            >
+              <circle cx="12" cy="12" r="5" />
+              <path d="M12 1v2M12 21v2M4.2 4.2l1.4 1.4M18.4 18.4l1.4 1.4M1 12h2M21 12h2M4.2 19.8l1.4-1.4M18.4 5.6l1.4-1.4" />
+            </svg>
+            <svg
+              className="col-start-2 row-start-1 stroke-base-100 fill-base-100"
+              xmlns="http://www.w3.org/2000/svg"
+              width="14"
+              height="14"
+              viewBox="0 0 24 24"
+              fill="none"
+              stroke="currentColor"
+              strokeWidth="2"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+            >
+              <path d="M21 12.79A9 9 0 1 1 11.21 3 7 7 0 0 0 21 12.79z"></path>
+            </svg>
+          </label>
+        </div>
+        <div>
+          <TfiWrite />
+        </div>
         <button className="btn btn-ghost btn-circle">
           <div className="indicator">
             <svg
