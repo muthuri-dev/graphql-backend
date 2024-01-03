3229911f5f8b6ea86fb36914094f7c167bc125db
muthuri-dev <muthurikennedy082@gmail.com>
2024-01-03 04:58:24 +0300

    Navbar unit tests

---
+++ __test__/components/shared/Navbar.test.tsx
@@ -0,0 +1,9 @@
+import { render, screen } from "@testing-library/react";
+import "@testing-library/jest-dom";
+import Navbar from "@/components/shared/Navbar";
+
+describe("NavBar page", () => {
+  it("", () => {
+    render(<Navbar />);
+  });
+});
